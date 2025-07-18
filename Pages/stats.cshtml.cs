d034dc464508345dd80069bf0ce08d7d7ce90986
wadah7836 <nooraliraqw@gmail.com>
2025-07-18 18:10:29 +0300

    Delete Pages directory

--- Pages/stats.cshtml.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-public class StatsModel : PageModel
-{
-    public void OnGet()
-    {
-        // لا حاجة لمنطق C# هنا، كل شيء يتم من خلال JavaScript
-    }
-}
